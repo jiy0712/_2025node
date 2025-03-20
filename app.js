c2cf5de9310fb2a9a8925609f945549faaf42e96
Another Name <another-email@example.com>
2025-03-21 08:57:40 +0900

    post 요청

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const express = require('express');
 // express 애플리케이션을 생성합니다.
 const app = express();
 
-// 루트 경로('/')에 대한 GET 요청을 처리합니다.
-app.get('/', (req, res) => {
+app.get('/swag', (req, res) => {
   // 응답 본문에 'Hello, World!'를 보냅니다.
-  res.send('Hello, World!');
+  res.send('get swag');
 });
+
+app.post('/swag', (req, res) => {
+  // 응답 본문에 'Hello, World!'를 보냅니다.
+  res.send('post swag');
+});
+
 
 // 서버가 포트 3000에서 요청을 대기합니다.
 app.listen(3000, () => {
