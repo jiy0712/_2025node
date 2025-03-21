fd127ad3f8bacc3d691548605e2d94c6ee56b2de
Another Name <another-email@example.com>
2025-03-21 09:57:12 +0900

    Router로 파일을 별도 관리

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
 // express 모듈을 가져옵니다.
 const express = require('express');
+const swagRouter = require('./routes/swag');
 
 // express 애플리케이션을 생성합니다.
 const app = express();
 
 app.use(express.json());
-
-app.get('/swag', (req, res) => {
-  res.send('get swag');
-});
-
-app.get('/swag/:person', (req, res) => {
-  res.send(req.params.person);
-});
-
-app.post('/swag', (req, res) => {
-  res.send(req.body);
-});
-
-
-
+app.use('/swag', swagRouter);
 
 // 서버가 포트 3000에서 요청을 대기합니다.
 app.listen(3000, () => {
