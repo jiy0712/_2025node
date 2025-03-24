778f88633027e10e378d825f9be43fafb1ea8326
jiyoung <s2325@e-mirim.hs.kr>
2025-03-24 10:41:56 +0900

    ejs

--- app2.js
+++ app2.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const app = express();
 
+travelList = ['뉴욕', '파리', '서울', '도쿄'];
+
 app.set('view engine', 'ejs');
 // __dirname : 현재 파일이 속한 절대경로
 // path.join을 사용하면 운영체제에 맞추어 경로 구분자(/, \)를 알아서 정해준다
@@ -16,3 +18,8 @@ app.get('/travel', (req, res) => {
     res.render('travel', {travelList});
 });
 // console.log(path.join(__dirname, 'views'));
+
+app.listen(3001, () => {
+    console.log('서버가 http://localhost:3001에서 실행 중입니다.');
+  });
+  
