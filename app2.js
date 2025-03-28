1e9dd4a1e3c8f29d15d4c551ba9a4856e43aff6f
jiyoung <s2325@e-mirim.hs.kr>
2025-03-28 09:50:02 +0900

    MySQL 연결

--- app2.js
+++ app2.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const path = require('path');
-
+const mysql = require('mysql2');
 const app = express();
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '0000',
+  database: 'traveldb'
+})
 
 travelList = ['뉴욕', '파리', '서울', '도쿄'];
 
@@ -10,16 +17,23 @@ app.set('view engine', 'ejs');
 // path.join을 사용하면 운영체제에 맞추어 경로 구분자(/, \)를 알아서 정해준다
 app.set('views', path.join(__dirname, 'views'));
 
+db.connect(err=>{
+  if(err){
+    console.err('MySQL 연결 실패 : ', err);
+    return;
+  }
+  console.log('MySQL에 연결되었습니다. '); 
+})
+
 app.get('/', (req, res) => {
-    res.render('home');
+  res.render('home');
 });
 
 app.get('/travel', (req, res) => {
-    res.render('travel', {travelList});
+  res.render('travel', { travelList });
 });
 // console.log(path.join(__dirname, 'views'));
 
 app.listen(3001, () => {
-    console.log('서버가 http://localhost:3001에서 실행 중입니다.');
-  });
-  
+  console.log('서버가 http://localhost:3001에서 실행 중입니다.');
+});
