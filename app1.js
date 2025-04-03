9ffeb06c17f9118b040e715c6bb7c1e336277096
jiyoung <s2325@e-mirim.hs.kr>
2025-04-04 08:42:15 +0900

    app.use

--- app1.js
+++ app1.js
@@ -66,6 +66,6 @@ app.use('/swag', swagRouter);
 app.use('/home', homeRouter);
 
 // 서버가 포트 3000에서 요청을 대기합니다.
-app.listen(3000, () => {
-  console.log('서버가 http://localhost:3000에서 실행 중입니다.');
-});
+// app.listen(3000, () => {
+//   console.log('서버가 http://localhost:3000에서 실행 중입니다.');
+// });
