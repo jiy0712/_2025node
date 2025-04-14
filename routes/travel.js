a8a192f040a005ea565fc3735ae9d1fbb64cdc47
Another Name <another-email@example.com>
2025-04-14 11:07:30 +0900

    라우팅 순서 정리

--- routes/travel.js
+++ routes/travel.js
@@ -2,10 +2,7 @@ const express = require('express');
 const db = require('../db');
 const router = express.Router();
 
-router.get('/add', (req, res) => {
-  res.render('addTravel');
-});
-
+// 게시글 목록을 보여줌
 router.get('/', (req, res) => {
   const _query = 'SELECT id, name FROM travellist';
   db.query(_query, (err, results) => {
@@ -19,6 +16,26 @@ router.get('/', (req, res) => {
   });
 });
 
+// 게시글 추가하는 페이지를 보여줌
+router.get('/add', (req, res) => {
+  res.render('addTravel');
+});
+
+// 게시글을 추가
+router.post('/', (req, res) => {
+  const { name } = req.body;
+  const _query = 'INSERT INTO travellist (name) VALUES (?)';
+  db.query(_query, [name], (err, results) => {
+    if (err) {
+      console.error('데이터베이스 쿼리 실패');
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    res.redirect('/travel');
+  });
+});
+
+// 게시글 내용 읽기
 router.get('/:id', (req, res) => {
   const travelID = req.params.id;
   const _query = 'SELECT * FROM travellist WHERE id = ?';
@@ -37,19 +54,26 @@ router.get('/:id', (req, res) => {
   });
 })
 
-router.post('/', (req, res) => {
-  const { name } = req.body;
-  const _query = 'INSERT INTO travellist (name) VALUES (?)';
-  db.query(_query, [name], (err, results) => {
+// 게시글을 수정하는 페이지를 보여줌
+router.get('/:id/edit', (req, res) => {
+  const travelID = req.params.id;
+  const _query = 'SELECT * FROM travellist WHERE id = ?';
+  db.query(_query, [travelID], (err, results) => {
     if (err) {
-      console.error('데이터베이스 쿼리 실패');
-      res.status(500).send('Internal Server Error');
+      console.error('DB 쿼리 실패', err);
+      res.status(500).send('내부 서버 에러');
       return;
     }
-    res.redirect('/travel');
+    if (results.length === 0) {
+      res.status(404).send('여행지를 찾을 수 없습니다');
+      return;
+    }
+    const travel = results[0];
+    res.render('editTravel', { travel });
   });
-});
+})
 
+// 게시글을 수정
 router.put('/:id', (req, res) => {
   const travelID = req.params.id;
   const { name } = req.body;
@@ -68,6 +92,7 @@ router.put('/:id', (req, res) => {
   });
 })
 
+// 게시글을 삭제
 router.delete('/:id', (req, res) => {
   const travelID = req.params.id;
   const _query = 'DELETE FROM travellist WHERE id = ?';
@@ -85,22 +110,5 @@ router.delete('/:id', (req, res) => {
   });
 })
 
-router.get('/:id/edit', (req, res) => {
-  const travelID = req.params.id;
-  const _query = 'SELECT * FROM travellist WHERE id = ?';
-  db.query(_query, [travelID], (err, results) => {
-    if (err) {
-      console.error('DB 쿼리 실패', err);
-      res.status(500).send('내부 서버 에러');
-      return;
-    }
-    if (results.length === 0) {
-      res.status(404).send('여행지를 찾을 수 없습니다');
-      return;
-    }
-    const travel = results[0];
-    res.render('editTravel', { travel });
-  });
-})
 
 module.exports = router;
